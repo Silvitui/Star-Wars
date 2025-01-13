920c7e05814325247dacc7d5a54ddca74270fa20
Silvitui <silviatuiranb@gmail.com>
2025-01-13 01:10:40 +0100

    starship list commit

--- src/app/starship-list/starship-list.component.ts
+++ src/app/starship-list/starship-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StarWarsService } from '../services/star-wars.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Starship } from '../interfaces/starship';
 
 @Component({
   standalone:true,
@@ -11,18 +12,18 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./starship-list.component.scss']
 })
 export class StarshipListComponent implements OnInit {
-  starships!: typeof this.starWarsService.starships;
-  totalPages!: typeof this.starWarsService.totalPages;
-  currentPage!: typeof this.starWarsService.currentPage;
+  starships: Starship[] = []
+  totalPages: number = 0;
+  currentPage: number = 1;
 
   constructor(public starWarsService: StarWarsService) {}
 
   ngOnInit(): void {
-    this.starships = this.starWarsService.starships;
-    this.totalPages = this.starWarsService.totalPages;
-    this.currentPage = this.starWarsService.currentPage;
 
     this.loadPage(1);
+    this.starWarsService.loadStarships().then((starships: Starship[]) => {
+      this.starships = starships;
+    })
   }
 
   loadPage(page: number): void {
