9c699c8758abe974ae753cacd73dc771a6c2365b
Silvia Tuiran <silviatuiranb@gmail.com>
2025-02-11 00:17:31 +0100

    feat: genera environment.prod.ts dinámico

---
+++ set-environment.js
@@ -0,0 +1,24 @@
+import { writeFile } from 'fs';
+
+const firebaseApiKey = process.env.FIREBASE_API_KEY || 'default-firebase-key';
+const firebaseAuthDomain = process.env.FIREBASE_AUTH_DOMAIN || 'default-auth-domain';
+const firebaseProjectId = process.env.FIREBASE_PROJECT_ID || 'default-project-id';
+
+const environmentFileContent = `
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '${firebaseApiKey}',
+    authDomain: '${firebaseAuthDomain}',
+    projectId: '${firebaseProjectId}'
+  }
+};
+`;
+
+writeFile('./src/environments/environment.prod.ts', environmentFileContent, (err) => {
+  if (err) {
+    console.error('❌ Error al generar environment.prod.ts:', err);
+  } else {
+    console.log('✅ environment.prod.ts generado con éxito.');
+  }
+});
